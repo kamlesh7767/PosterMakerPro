478f14a4eeaeed15f89fb64653577151a354c5b5
kamlesh7767 <pardeshikamlesh61@gmail.com>
2023-07-24 13:15:43 +0530

    kamlesh

---
+++ app/src/main/java/com/garudpuran/postermakerpro/ui/commonui/HomeCategoryModel.kt
@@ -0,0 +1,7 @@
+package com.garudpuran.postermakerpro.ui.commonui
+
+class HomeCategoryModel(
+    val id:Int,
+    val title:String,
+    val icon_res:Int
+)
---
+++ app/src/main/java/com/garudpuran/postermakerpro/ui/commonui/HomeResources.kt
@@ -0,0 +1,5 @@
+package com.garudpuran.postermakerpro.ui.commonui
+
+class HomeResources {
+
+}
