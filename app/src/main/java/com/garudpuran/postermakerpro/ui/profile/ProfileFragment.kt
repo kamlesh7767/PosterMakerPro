85ab6809e0f079fa9d6f6933a814b596a3705c92
Rushikesh Pardeshi <112465300+MobilefirstRushikesh@users.noreply.github.com>
2023-07-25 13:00:13 +0530

    25-07  some otp flow fixes and profile fragment ui.

--- app/src/main/java/com/garudpuran/postermakerpro/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/garudpuran/postermakerpro/ui/profile/ProfileFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.garudpuran.postermakerpro.databinding.FragmentNotificationsBinding
+import com.garudpuran.postermakerpro.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +18,8 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onDestroyView() {
